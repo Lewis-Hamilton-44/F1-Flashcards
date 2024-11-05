fcb1dfff3d9981806685f62fa6a0ff40fc44a6ba
Lewis-Hamilton-44 <00001114741553sp@al.educacao.sp.gov.br>
2024-11-05 15:09:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'TITULOS',
+    'Em que ano, Lewis hamilton venceu seu primeiro titulo mundil de pilotos?',
+    '2008'
+)
+
+criaCartao(
+    'ERA',
+    'Qual piloto de F1 veneu o campeonato mundial de 2010-2013?',
+    'Sebastian Vettel'
+)
+
+criaCartao(
+    'PRODÍGIO',
+    'Qual foi o piloto mais novo a entrar na F1??',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'LEGADO',
+    'Qual é o piloto que rem o apelido de Mestre da chuva?',
+    'Ayrton Senna'
+)
